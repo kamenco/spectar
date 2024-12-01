db9da3c6ccbf3d33baa3116f59b6e16022ea7e30
Kamen Kirov <kamen@bgtown.com>
2024-12-01 19:39:23 +0000

    Added: functionality, uploading image to the user profile page.

--- avertise/upload/urls.py
+++ avertise/upload/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'upload'  # Register the namespace
+
 urlpatterns = [
     path('upload/<int:order_id>/', views.upload_completed_work, name='upload_completed_work'),
     path('all-orders/', views.view_all_orders, name='view_all_orders'),
 ]
-
